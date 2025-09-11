58b80f26616d4b8b13081ffdd8689fd9c3a691e4
gerikov <gerikov90@gmail.com>
2025-09-11 09:38:12 +0200

    change social text and add robots.txt

--- src/components/features/landing/Socials.tsx
+++ src/components/features/landing/Socials.tsx
@@ -6,10 +6,14 @@ const Socials = () => {
   return (
     <section className='bg-primary py-12 mt-20 px-2'>
       <div className='container flex flex-col gap-4 mx-auto'>
-        <div className='text-2xl font-bold text-black'>Are you interested, but still have a question?</div>
-        <div className='text-xl font-bold md:max-w-1/2'>
-          We are more than happy to answer all of those questions on every social platform.
+        <div className='text-2xl font-bold text-white md:max-w-1/2'>
+          Follow us on social media to get real-time updates and
+          behind-the-scenes insights into our project.
         </div>
+        {/* <div className='text-xl font-bold md:max-w-1/2'>
+          We are more than happy to answer all of those questions on every
+          social platform.
+        </div> */}
         <div className='flex  text-black'>
           <SocialContentWithLoading />
         </div>
@@ -22,7 +26,12 @@ const SocialContent = ({ socials }: { socials: Social[] }) => {
   return (
     <div className='flex flex-wrap gap-4 max-w-2xl justify-center'>
       {socials.map((social) => (
-        <a key={social.id} href={social.link} target='_blank' rel='noopener noreferrer'>
+        <a
+          key={social.id}
+          href={social.link}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           <img
             src={social.icon.url}
             alt={social.platform}
@@ -39,7 +48,14 @@ const SocialContent = ({ socials }: { socials: Social[] }) => {
 const SocialContentWithLoading = () => {
   const { isLoading, error, refetch, data } = useSocial()
   const WrappedSocialContent = withLoading(SocialContent)
-  return <WrappedSocialContent isLoading={isLoading} error={error} refetch={refetch} socials={data || []} />
+  return (
+    <WrappedSocialContent
+      isLoading={isLoading}
+      error={error}
+      refetch={refetch}
+      socials={data || []}
+    />
+  )
 }
 
 export default Socials
