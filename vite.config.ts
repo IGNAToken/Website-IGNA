3bc914812cfcd54218ba627c2ba51d9f32ea5c31
gerikov <developer@gerikov.hu>
2025-12-01 21:50:54 +0100

    [FIX] netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    netlify(),
   ],
   resolve: {
     alias: {
